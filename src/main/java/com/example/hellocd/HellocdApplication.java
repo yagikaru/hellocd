5e65ef6d081c13d0a5ab09c891e56eaea4e25fb3
student <evan@prometheus.systems>
2023-07-23 16:08:31 +0000

    committing changes to pom.xml andHellocdApplication.java

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Continuous Delivery!";
 	}
 
 	public static void main(String[] args) {
